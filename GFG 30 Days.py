69a31ed80de273de89e3b0ad6533c1dddc71cb29
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-01-30 13:22:10 +0530

    Update GFG 30 Days.py
    Geeks for Geeks Jan 2021 - Feb 2021
    30 days Practice Problems for Python with solutions

--- GFG 30 Days.py
+++ GFG 30 Days.py
@@ -87,8 +87,53 @@ class Solution:
             return 2*(k-1) + n + 1;
             
         return 2*sock + n + (k-sock);
-            
-        
+       
+       
+      #In[ ]
+      
+  Spiral Matrix
+  Given a matrix of size N x M. You have to find the Kth element which will obtain while traversing the matrix spirally starting from the top-left corner of the matrix.
+  
+    class Solution:
+      def Spiral_matrix:
+      
+        top = 0
+        left = 0
+        right = m-1;
+        bottom = n-1;
+        dir =0;
+        count=0;
+        while(top<=bottom and left<=right):
+            if(dir==0):
+                for i in range(left, right+1):
+                    k -= 1
+                    if k == 0:
+                        return (a[top][i])
+                top += 1
+                dir=1
+                    
+            if(dir==1):
+                for i in range(top, bottom+1):
+                    k -= 1
+                    if k == 0:
+                        return (a[i][right])
+                right -= 1
+                dir=2
+            if(dir==2):
+                for i in range(right, left - 1, -1):
+                    k -= 1
+                    if k == 0:
+                        return (a[bottom][i])
+                bottom -= 1
+                dir=3
+            if(dir==3):
+                for i in range(bottom, top - 1, -1):
+                    k -= 1
+                    if k == 0:
+                        return (a[i][left])
+                left += 1
+                dir=0
+
             
 
  

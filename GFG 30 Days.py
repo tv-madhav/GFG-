61c0fa831467cf7c6d0210fa0cfbdbbee94a6d68
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-04 06:55:44 +0530

    Update GFG 30 Days.py

--- GFG 30 Days.py
+++ GFG 30 Days.py
@@ -266,3 +266,22 @@ Note: Coins of the negative denomination are also possible at Geekland.
 
           return ans
 
+ #In[10]     
+         
+       Repeated String Match
+   
+       Given two strings A and B, find the minimum number of times A has to be repeated such that B becomes a substring of the repeated A. 
+        If B cannot be a substring of A no matter how many times it is repeated, return -1. 
+         class Solution:
+             def repeatedStringMatch(self, A, B):
+                 # code here
+                 a = A
+                 ans = 1
+                 while len(a)<len(B):
+                     a +=A
+                     ans +=1
+                 if B in a:
+                     return ans
+                 if B in a + A :
+                     return ans + 1
+                 return -1

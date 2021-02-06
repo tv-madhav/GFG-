abd4b784a88a94a0f4c2dd6338ba7dfbabb53645
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-06 19:42:58 +0530

    Update GFG 30 Days(11 - 20 Days ).py

--- GFG 30 Days(11 - 20 Days ).py
+++ GFG 30 Days(11 - 20 Days ).py
@@ -81,3 +81,29 @@ class Solution:
             n=n-1
         return ret
     
+#In[12]
+
+
+     Bit Difference 
+    
+    
+   Given an integer array of size  N . You have to find sum of bit differences in all pairs that can be formed from array elements. 
+    Bit difference of a pair (x, y) is count of different bits at same positions in binary representations of x and y.
+    For example, bit difference for 2 and 7 is 2. Binary representation of 2 is 010 and 7 is 111 ( first and last bits differ in two numbers). 
+    
+            class Solution:
+                def sumBitDiff(self, arr, n): 
+                    # Your code goes here
+                    ans = 0
+                    for i in range(0,32):
+                        count = 0
+                        for j in range(0,n):
+                             if ( (arr[j] & (1 << i)) ): 
+                                count+=1
+
+                        # Add "count * (n - count) * 2" to the answer 
+                        ans += (count * (n - count) * 2); 
+
+                    return ans
+
+

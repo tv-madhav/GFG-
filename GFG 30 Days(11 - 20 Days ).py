1c7be7aaf39347e4ffda92567eb0d8bbf344b9d8
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-07 00:12:04 +0530

    Update GFG 30 Days(11 - 20 Days ).py

--- GFG 30 Days(11 - 20 Days ).py
+++ GFG 30 Days(11 - 20 Days ).py
@@ -84,7 +84,10 @@ class Solution:
 #In[12]
 
 
-     Bit Difference 
+Check Tree Traversal
+
+Given Preorder, Inorder and Postorder traversals of some tree of size N. The task is to check if they are all of the same tree or not.
+    
     
     
    Given an integer array of size  N . You have to find sum of bit differences in all pairs that can be formed from array elements. 
@@ -105,5 +108,42 @@ class Solution:
                         ans += (count * (n - count) * 2); 
 
                     return ans
+                
+                
+   #In[13]             
+                
+            
+            
+            class Solution:
+                def checktree(self, preorder, inorder, postorder, N): 
+
+
+                    if N == 0: 
+                        return 1
+
+                    if N == 1: 
+                        return (preorder[0] == inorder[0]) and (inorder[0] == postorder[0]); 
+
+                    idx = -1; 
+
+                    for i in range(N): 
+                        if inorder[i] == preorder[0]: 
+                            idx = i 
+                            break
+
+                    if idx == -1: 
+                        return 0;
+
+                    if preorder[0] != postorder[N-1]:
+                        return 0;
+
+
+                    ret1 = self.checktree(preorder[1:], inorder, postorder, idx); 
+
+                    ret2 = self.checktree(preorder[idx + 1:], inorder[idx + 1:], 
+                                        postorder[idx:], N-idx-1); 
+
+                    return (ret1 and ret2) 
+
 
 

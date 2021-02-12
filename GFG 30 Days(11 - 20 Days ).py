fe7ba5a47db66decfa8d9d9348ddf393051f38d4
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-12 13:11:20 +0530

    Update GFG 30 Days(11 - 20 Days ).py

--- GFG 30 Days(11 - 20 Days ).py
+++ GFG 30 Days(11 - 20 Days ).py
@@ -272,6 +272,51 @@ Note: one student may be selected by multiple classmates.
                         stack.append(i)
 
                     return ans 
-            
+       
+    
+    
+   #In[18]
+
+132 Geeky Buildings (Topic : Stack)
+
+There are N buildings in Linear Land. They appear in a linear line one after the other and their heights are given in the array arr[]. 
+Geek wants to select three buildings in Linear Land and remodel them as recreational spots. 
+The third of the selected building must be taller than the first and shorter than the second.
+Can geek build the three-building recreational zone? 
+
+            from math import inf
+
+            class Solution:
+                def recreationalSpot(self, arr, n):
+                    l=[]
+
+                    if len(arr)<=2:
+                        return False
+                    if len(arr)==3:
+                        if arr[0]<arr[2]<arr[1]:
+                            return True
+                        else:
+                            return False
+
+                    if arr[0] < arr[2] < arr[1]:
+                        return True
+
+                    temp=[inf]*n
+
+                    temp[0]=arr[0]
+
+                    for i in range(n):
+                        temp[i]=min(temp[i-1],arr[i])
+
+                    for j in range(n-1,-1,-1):
+                        if arr[j]>temp[j]:
+                            while l and l[-1]<=temp[j]:
+                                l.pop()
+
+                            if l and l[-1]<arr[j]:
+                                return True
+                            l.append(arr[j])
+                    return False
+
 
 

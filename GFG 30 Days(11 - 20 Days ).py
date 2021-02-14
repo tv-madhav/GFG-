dcfb58e651f1e45d8a92676b266708cae7a643f9
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-14 17:37:19 +0530

    Update GFG 30 Days(11 - 20 Days ).py

--- GFG 30 Days(11 - 20 Days ).py
+++ GFG 30 Days(11 - 20 Days ).py
@@ -356,5 +356,53 @@ Note: The result is always finite as after a point X every index in the infinite
                             set.add(key)
 
                     return count 
+                
+                
+        #In[20]
+
+        Valentine Sum 
+
+        Cupid wants to strike maximum houses in Geek Land on Valentine's Day. 
+        The houses in Geek Land are arranged in the form of a binary tree. Cupid is standing at target node initially. 
+        Find the sum of all nodes within a maximum distance k from target node. The target node should be included in the sum.
+
+
+
+           class Solution:
+                def add_subtree(self, n, dist):
+                    if n is None or dist<0:
+                        return 0
+                    return n.data + self.add_subtree(n.left,dist-1) + self.add_subtree(n.right,dist-1)
+
+                def traverse(self, n ,target, k):
+                    if n is None:
+                        return (0,-1)
+                    if n.data==target:
+                        return (self.add_subtree(n,k), k-1)
+                        # adding all nodes within range in the sub tree below
+                        # and returning sum
+
+                    Sum,dist = self.traverse(n.left, target, k)
+                    if Sum>0:
+                        # Sum>0 indicates target was found in left subtree
+                        if dist==-1:
+                            return (Sum,dist)
+                        return ( Sum+n.data + self.add_subtree(n.right,dist-1) , dist-1 )
+                        # adding values from right sub tree
+
+                    Sum,dist = self.traverse(n.right, target, k)
+                    if Sum>0:
+                        # Sum>0 indicates target was found in right subtree
+                        if dist==-1:
+                            return (Sum,dist)
+                        return ( Sum+n.data + self.add_subtree(n.left,dist-1) , dist-1 )
+                        # adding values from left sub tree
+
+                    return (0,-1)
+
+                def sum_at_distK(self, root, target, k):
+                    Sum,dist = self.traverse(root, target, k)
+                    return Sum
+
 
 

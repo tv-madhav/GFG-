ca894654f3b45c21557d97b0bda450e17c484faf
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-16 09:08:11 +0530

    Update GFG 30 Days(21 - 30 Days ).py

--- GFG 30 Days(21 - 30 Days ).py
+++ GFG 30 Days(21 - 30 Days ).py
@@ -34,5 +34,86 @@ class Solution:
         if dist==3:
             return vaccineKit+1
         return vaccineKit
+    
+    
+ #In[22]
+
+
+Shortest Range In BST 
+
+Given a BST (Binary Search Tree), find the shortest range [x, y], such that, at least one node of every level of the BST lies in the range.
+If there are multiple ranges with the same gap (i.e. (y-x)) return the range with the smallest x.
+
+class Solution:
+    
+    def storein(self, root, lvl, lin, llv):
+        if not root:
+            return
+        self.storein(root.left,lvl+1,lin,llv)
+        lin.append(root.data)
+        llv.append(lvl)
+        self.storein(root.right,lvl+1,lin,llv)
+        
+        
+        
+    def shortestRange(self, root):
+
+        inorder=[]
+        level=[]
+        self.storein(root,0,inorder,level)
+        i=j=k=cntzero=0
+        maxDepth=max(level)+1
+        #  stores number of nodes at ith level
+        depth=[0]*maxDepth
+        # first count number of nodes at ith level till the root 
+        #  right pointer initially is at root's index in inorder traversal of bst
+        for k in range(len(level)):
+            depth[level[k]]+=1
+            if level[k]==0:
+                j=k
+                break
+        #  count number of levels where there are 0 nodes in the range inorder[i] to inorder[j]
+        cntzero=depth.count(0)
+        #  intially shortest range is [x, y]
+        #  x=node at 0th index
+        #  y=node at last index
+        #  i.e. the whole tree
+        x,y=inorder[0],inorder[-1]
+        # if currently picked range contains all levels change x and y accordingly
+        if cntzero==0:
+            x,y=inorder[i],inorder[j]
+    
+        # left pointer can at most go upto root's index(i.e. k)
+        # right pointer can go upto last index of inorder traversal of tree
+        while i<=k and j<len(inorder):
+            #  while right pointer doesn't reach last index 
+            #  and the current range doesn't contain all levels
+            while j<len(inorder):
+                #  if cntZero is 0 then this range contains all levels
+                if cntzero==0:
+                    #  if previous range is large then change the range
+                    if (y-x) > (inorder[j]-inorder[i]):
+                        x,y=inorder[i],inorder[j]
+                    break
+                j+=1
+    
+                if j>= len(inorder):
+                    break
+                #  if new level is discovered by this range then cntZero is decreased by 1
+                if depth[level[j]]==0:
+                    cntzero-=1
+                depth[level[j]]+=1
+            #  while current range contains all levels 
+            # we can shift the left pointer by +1
+            while not cntzero and i<=k:
+                if (y-x)>(inorder[j]-inorder[i]):
+                    x,y=inorder[i],inorder[j]
+                depth[level[i]]-=1
+                #  if this level is outside the current range then cntZero is increased by 1
+                if depth[level[i]]==0:
+                    cntzero+=1
+                i+=1
+        return (x,y)
+
 
 

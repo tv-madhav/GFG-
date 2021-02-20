3ad2f61e36fcde1bec2324e8f5e1865fe74d4ff8
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-20 09:02:06 +0530

    Update GFG 30 Days(21 - 30 Days ).py

--- GFG 30 Days(21 - 30 Days ).py
+++ GFG 30 Days(21 - 30 Days ).py
@@ -189,3 +189,99 @@ class Solution:
                         q.append((nextr,nextc))
         return ret
         
+        
+        
+        
+     #In[26]
+    
+    Project Manager 
+   
+   An IT company is working on a large project. The project is broken into N modules and distributed to different teams. 
+    The amount of time (in months) required to complete each module is given in an array duration[ ]. Two modules can be processed simultaneously 
+    only if their is no dependency between them and it is given that M modules have interdependecies. 
+    As the project manager, compute the minimum time required to complete the project. 
+    
+
+from collections import defaultdict
+
+class Solution:
+    def checkCycle(self, i):
+        global isstack
+        global visited
+        global counted
+        global ve
+        if (isstack[i] == 1):
+            return rue
+        # insert into stack
+        isstack[i] = 1
+        for it in ve[i]:
+            # if next node is visited
+            if (visited[it] == 1):
+                # if the node is in stack then cycle is found
+                if (isstack[it] == 1):
+                    return True
+                continue
+            visited[it] = 1
+            if (self.checkCycle(it)==True):
+                return True;
+        # removing from stack   
+        isstack[i] = 0
+        return False
+    
+    def dfs(self, i, duration):
+        global counted
+        global visited
+        global isstack
+        global ve
+        #  return the time to complete the project starting from the node i if it is
+        #  already calculated
+        if (counted[i] != -1):
+            return counted[i]
+        x = 0;
+    
+        for it in ve[i]:
+            x = max(x, self.dfs(it, duration))
+        #  time to complete this module and maximum time to complete child modules
+        counted[i] = x + duration[i]
+        return counted[i]
+    
+    def minTime(self, vp, duration, n, m):
+        independent=[0]*(n+5)
+        global ve
+        ve = defaultdict(lambda:[])
+        for i in range(0,m):
+            x = vp[i][0]
+            y = vp[i][1]
+            ve[x].append(y)
+            independent[y]+=1
+        global visited
+        global counted
+        global isstack
+        visited =[-1]*(n+5)
+        counted =[-1]*(n+5)
+        isstack =[-1]*(n+5)
+        
+        flag = 0;
+        for i in range(0,n):
+            if (independent[i] == 0):
+                flag = 1
+        # no independent module
+        if (flag == 0):
+            return -1
+    
+        for i in range(0,n):
+            if (independent[i] != 0):
+                continue
+            visited[i] = 1
+            # checking cycle 
+            if (self.checkCycle(i)):
+                return -1
+        
+        ans = 0;
+        # starting from any independent module find the maximum time to complete
+        # the project
+        for i in range(0,n):
+            if (independent[i] == 0):
+                ans = max(ans, self.dfs(i, duration));
+    
+        return ans;

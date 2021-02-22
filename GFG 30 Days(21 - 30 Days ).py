5e95a17d63eb774828290ca802d5bab47cd880a6
Vamsi Madhav <60893588+tv-madhav@users.noreply.github.com>
2021-02-22 08:40:02 +0530

    Update GFG 30 Days(21 - 30 Days ).py

--- GFG 30 Days(21 - 30 Days ).py
+++ GFG 30 Days(21 - 30 Days ).py
@@ -287,7 +287,7 @@ class Solution:
         return ans
     
     
-#In[26]
+#In[27]
 
 Police and Thives
 
@@ -327,3 +327,39 @@ class Solution:
                 r +=1
                 
         return result
+
+    #[28]
+    
+    Water the plants
+    
+    A gallery with plants is divided into n parts, numbered : 0,1,2,3...n-1. There are provisions for attaching water sprinklers at every partition.
+        A sprinkler with range x at partition i can water all partitions from i-x to i+x.
+    Given an array gallery[ ] consisting of n integers, where gallery[i] is the range of sprinkler at partition i (power==-1 indicates no sprinkler attached), 
+    return the minimum number of sprinklers that need to be turned on to water the complete gallery.
+    If there is no possible way to water the full length using the given sprinklers, print -1.
+    
+    
+    class Solution:
+    def min_sprinklers(self, gallery, n):
+        # code here
+        sprinklers = []
+        for i in range(n):
+            if gallery[i]>-1:
+                sprinklers.append( [i-gallery[i] , i+gallery[i]] );
+        sprinklers.sort()
+        target=0
+        sprinklers_on=0
+        i=0
+        while(target<n):
+            if i==len(sprinklers) or sprinklers[i][0]>target:
+                return -1
+            max_range = sprinklers[i][1]
+            while( i+1<len(sprinklers) and sprinklers[i+1][0]<=target ):
+                i+=1
+                max_range = max(max_range,sprinklers[i][1])
+            if max_range<target:
+                return -1
+            sprinklers_on+=1
+            target=max_range+1
+            i+=1
+        return sprinklers_on
